59606f0ed578f55d3b3d078aff08b00a949e5738
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2025-11-30 15:43:04 +0800

    Implementing Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import { isDesktopHero, isMobilHero } from "../utils"
+
+function Hero() {
+
+    const [heroSrc, setHeroSrc] = useState( window.innerWidth < 730 ? isMobilHero : isDesktopHero );
+
+    const handleHeroSrcSet = () => {
+        if(window.innerWidth < 730){
+            setHeroSrc(isMobilHero);
+        } else {
+            setHeroSrc(isDesktopHero);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleHeroSrcSet);
+        return () => {
+            window.removeEventListener('resize', handleHeroSrcSet)
+        }
+    }, [])
+
+
+  return (
+    <section id="hero">
+        <div className="hero-content">
+            <img src={heroSrc} alt="Hero" className="screenHeightMedium screenHeightFull screenHeightSmall" />
+        </div>
+        <div className="hero-footer">
+            <a href="">Buy</a>
+            <p>From ₱79,990</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
